794aa572a278a17d06aee3c2c46f8a914c47b868
francisjosephironman <francis.3.6.joseph@gmail.com>
2022-11-21 10:58:47 +0800

    updated shortlisted candidates controller and index view

--- app/Http/Controllers/ShortlistedCandidateController.php
+++ app/Http/Controllers/ShortlistedCandidateController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ShortlistedCandidateController extends Controller
 {
     public function index()
     {
-        return view('company.shortlisted-candidates.index');
+        return view('company.shortlisted-candidates.index')
+            ->with('job_applications', Auth::user()->company->jobApplicationsReceived);
     }
 }
--- resources/views/company/shortlisted-candidates/index.blade.php
+++ resources/views/company/shortlisted-candidates/index.blade.php
@@ -1,13 +1,40 @@
 @extends('layouts.app')
     
 @section('customcss')
-    <link rel="stylesheet" href="/css/welcome.css">
+    <link rel="stylesheet" href="/css/company/shortlisted-candidates/index.css">
 @endsection
     
 @section('customjs')
-    <script src="/js/welcome.js" type="module"></script>
+    <script src="/js/company/shortlisted-candidates/index.js" type="module"></script>
 @endsection
 
 @section('content')
-    
+    <h1 class="text-center">SHORTLISTED CANDIDATES</h1>
+
+    <div class="table-responsive">
+        <table class="table table-dark text-center table-bordered mt-4">
+            <thead class="bg-dark text-white">
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Job Post</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($job_applications as $job_application)
+                    <tr>
+                        <td>{{ $job_application->student->last_name }}</td>
+                        <td>{{ $job_application->student->user->email }}</td>
+                        <td>{{ $job_application->jobPost->position }}</td>
+                        <td>
+                            <button type='button' class="btn btn-warning">Send invitation</button>
+                        </td>
+                    </tr>
+                @empty
+                    
+                @endforelse
+            </tbody>
+        </table>
+    </div>
 @endsection
