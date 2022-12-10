3008ef5890e15e826e1ee5f2c4d142b3910d7f89
francisjosephironman <francis.3.6.joseph@gmail.com>
2022-12-10 21:10:23 +0800

    created index view for admin

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+    
+@section('customcss')
+    <link rel="stylesheet" href="/css/admin/index.css">
+@endsection
+    
+@section('customjs')
+    <script src="/js/admin/index.js" type="module"></script>
+@endsection
+
+@section('content')
+    
+@endsection
